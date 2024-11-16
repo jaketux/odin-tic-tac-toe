0f16680c07f7d2a57fa1f5c4b5330920df288c72
jaketux <169253660+jaketux@users.noreply.github.com>
2024-11-16 20:16:24 +1100

    More progress towards tic tac toe!
    To do:
    
    - Reset gameboard array logic
    - Object to control the game flow including new game, checking for winners, checking for who's move it is, keeping score.
    - Run a full game in the console.
    - Create the UI.

--- script.js
+++ script.js
@@ -1,77 +1,78 @@
 
+const gameBoard = (function(){
 
-const gameBoardArray = 
-    ["O","O","X",
-    "X","X","O",
-    "O","O","O"];  
+    const gameBoardArray = [,,,,,,,,,]
 
-let counter = 0
+    let playersTurn = true
 
-function createPlayer(name){
-    const playerName = name;
-    const getValues = function(){
-        if (counter === 0){ 
-            token = "X"
-            number = 1;
-            counter++
-            return [token, number]
-        } else if (counter === 1){
-            token = "O"
-            number = 2;
-            counter = 0
-            return [token, number]
+    const placeToken = function(location, player){
+        if (player === players[0] && playersTurn === true){
+        gameBoardArray.splice(location, 1, "X"),
+        playersTurn = false,
+        console.log(gameBoardArray)}
+        else if (player === players[1] && playersTurn === false){
+        gameBoardArray.splice(location, 1, "O"),
+        playersTurn = true,
+        console.log(gameBoardArray)
+        } else {console.log("Error, it's not your turn!!!"),
+            console.log(gameBoardArray) 
         }
     }
-return {playerName, getValues}
+    console.log(gameBoardArray)
+
+    return {placeToken,gameBoardArray}    
+
+    })()
+
+function createPlayers(name1,name2){
+
+    function createPlayer(name){
+        const playerName = name;
+        return {playerName}
+    }
+
+    const player1 = createPlayer(name1)
+
+    const player2 = createPlayer(name2)
+
+    return players = [player1, player2]
+
 }
 
-const player1 = createPlayer('willis')
-player1.getValues()
+createPlayers("Paul", "Steve")
 
-const player2 = createPlayer('bruce')
-player2.getValues()
 
-console.log(player1.getValues()[0])
-console.log(player2.getValues()[1])
+console.log(players[0])
+console.log(players[1])
 
-// gameBoardArray.push(player1.getValues()[0])
 
-console.log(gameBoardArray[0])
-
-function checkWinner(){
-    if (gameBoardArray[0,1,2] || 
-        gameBoardArray[3,4,5] || 
-        gameBoardArray[6,7,8] || 
-        gameBoardArray[0,3,6] || 
-        gameBoardArray[1,4,7] || 
-        gameBoardArray[2,5,8] ||
-        gameBoardArray[0,4,8] || 
+const checkWinner = function(gameBoardArray){
+    if (gameBoardArray[0,1,2] === "X" || 
+        gameBoardArray[3,4,5] === "X" || 
+        gameBoardArray[6,7,8] === "X" || 
+        gameBoardArray[0,3,6] === "X" || 
+        gameBoardArray[1,4,7] === "X" || 
+        gameBoardArray[2,5,8] === "X" ||
+        gameBoardArray[0,4,8] === "X" || 
         gameBoardArray[2,4,6] === "X"){
             console.log(player1.playerName+" is the winner!")
     } else if (
-        gameBoardArray[0,1,2] || 
-        gameBoardArray[3,4,5] || 
-        gameBoardArray[6,7,8] || 
-        gameBoardArray[0,3,6] || 
-        gameBoardArray[1,4,7] || 
-        gameBoardArray[2,5,8] ||
-        gameBoardArray[0,4,8] || 
-        gameBoardArray[2,4,6] === "O"){
+        gameBoardArray[0,1,2] === "O" || 
+        gameBoardArray[3,4,5] === "O" || 
+        gameBoardArray[6,7,8] === "O" || 
+        gameBoardArray[0,3,6] === "O" || 
+        gameBoardArray[1,4,7] === "O" || 
+        gameBoardArray[2,5,8] === "O" ||
+        gameBoardArray[0,4,8] === "O" || 
+        gameBoardArray[2,4,6] === "O" ){
             console.log(player2.playerName+" is the winner!")
         }
 }    
 
-console.log(checkWinner())
 
-012
-345
-678
-036 
-147
-258
-048
-246
+// function checkWinner()
 
 
+gameBoard.placeToken(2,players[0])
 
 
